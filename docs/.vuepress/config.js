96730aa0723bae4d22c6e7e01267a02e645f96b5
xubaoshan <1411673883@qq.com>
2018-12-27 21:30:39 +0800

    pp

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,7 +18,7 @@ module.exports = {
           { text: 'vue', link: '/vue/test1' },
           { text: 'node.js', link: '/nodejs/1. Node.js 介绍' },
           { text: '微信小程序', link: '/wx/test1' },
-          { text: '重难点', link: '/exercise/syntax/11. promise' },
+          { text: '重难点', link: '/exercise/syntax/4. promise' },
           // 下拉列表的配置
           {
             text: '命令与工具',
--- docs/exercise/exSideBar.js
+++ docs/exercise/exSideBar.js
@@ -3,7 +3,10 @@ module.exports = [
       title: '知识点',
       collapsable: true,
       children: [
-        'syntax/11. promise'
+        'syntax/1. js堆栈与深浅拷贝',
+        'syntax/2. var 与 let',
+        'syntax/3. this 与 bind call apply',
+        'syntax/4. promise'
       ]
     }
 ]
