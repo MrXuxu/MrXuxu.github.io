fe46003e2afc43936233fee926a03208ba6b86b6
xubaoshan <1411673883@qq.com>
2018-12-28 14:58:19 +0800

    hh

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -79,7 +79,7 @@ module.exports = {
 
         // 在 github 上编辑此页
         // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-        repo: 'https://github.com/MrXuxu/blog',
+        repo: 'https://github.com/MrXuxu/MrXuxu.github.io',
         // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
         // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
         repoLabel: '查看源码',
@@ -87,9 +87,9 @@ module.exports = {
         // 以下为可选的编辑链接选项
 
         // 假如你的文档仓库和项目本身不在一个仓库：
-        docsRepo: 'https://github.com/MrXuxu/blog',
+        docsRepo: 'https://github.com/MrXuxu/MrXuxu.github.io',
         // 假如文档不是放在仓库的根目录下：
-        docsDir: 'docs',
+        // docsDir: 'docs',
         // 假如文档放在一个特定的分支下：
         docsBranch: 'master',
         // 默认是 false, 设置为 true 来启用
